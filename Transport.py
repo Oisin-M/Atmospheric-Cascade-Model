f6aa4fd684748b5954aec8b424746bfb780920b7
Oisin-M <oisinmmm@gmail.com>
2020-07-27 20:09:04 +0100

    added some data for the atmosphere

--- Transport.py
+++ Transport.py
@@ -1,5 +1,20 @@
 import numpy as np
 
+atmosphere_comp={
+'O2': 0.7809,
+'N2': 0.2095,
+'Ar': 0.0093
+} #https://en.wikipedia.org/wiki/Atmosphere_of_Earth for now
+
+new_percs=np.array([0.7809*2, 0.2095*2, 0.0093])
+new_percs*=1/new_percs.sum()
+
+air_data={
+8: new_percs[0],
+7: new_percs[1],
+18: new_percs[2]
+} #Z to % dict
+
 def get_cross_sections(particle):
 
     if particle.name=="photon":
@@ -18,7 +33,7 @@ def move(particle):
 
     sample_N_lambda = -np.log(np.random.random())
 
-    dx=sample_N_lambda*lambd
+    dx=sample_N_lambda*lambd #approximately
 
     cartesian_direction=np.array([np.sin(particle.direction[0])*np.cos(particle.direction[1]),np.sin(particle.direction[0])*np.sin(particle.direction[1]), np.cos(particle.direction[1])])
 
