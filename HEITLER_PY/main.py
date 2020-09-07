bdc56ca2e61679966e2ed15f6ea6bc98ff32c4d6
Oisin-M <oisinmmm@gmail.com>
2020-09-07 16:16:54 +0100

    small fix

--- HEITLER_PY/main.py
+++ HEITLER_PY/main.py
@@ -37,7 +37,7 @@ while len(stack)>0:
     else:
         if particle[2]/2<crit:
             logs.append([particle[0], particle[1], particle[2]/2, particle[3]+d])
-            logs.append([particle[0], "photon", particle[2]/2, particle[3]+d])
+            logs.append([last_id+1, "photon", particle[2]/2, particle[3]+d])
         else:
             stack.append([particle[0], particle[1], particle[2]/2, particle[3]+d])
             stack.append([last_id+1, "photon", particle[2]/2, particle[3]+d])
